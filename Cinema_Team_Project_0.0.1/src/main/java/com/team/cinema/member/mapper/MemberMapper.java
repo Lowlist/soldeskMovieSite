0ff2308414ee5286615c6307d55b5ead4a83cfeb
Lowiist <dud5825@naver.com>
2024-05-08 09:20:28 +0900

    리액트 재 업데이트 (깃 이그노어 삭제)

--- Cinema_Team_Project_0.0.1/src/main/java/com/team/cinema/member/mapper/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package com.team.cinema.member.mapper;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberMapper {
-	public void insertTest();
-	public String selectTest();
-}
