af45eb2770a6a0e9ca968d7fa9030dd1b94d8a14
dud5825@naver.com <dud5825@naver.com>
2024-06-28 16:53:40 +0900

    -패키지 상품 구현 완료 -음식 상품 구현 진행중
    Lowlist/soldeskMovieSite#3

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/shop/Goods.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/shop/Goods.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Card } from 'react-bootstrap';
 import Badge from 'react-bootstrap/Badge';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from './data';
@@ -7,24 +7,38 @@ import data from './data';
 function Goods() {
     let [shoes] = useState(data);
     return (
-        <Container>
-            <h3>
-                Example heading <Badge bg="secondary">New</Badge>
-            </h3>
-            <Row>
-                {
-                    shoes.map((a,i) => {
-                        return (
-                            <Col>
-                                <img src={'https://codingapple1.github.io/shop/shoes'+(i+1)+'.jpg'} width="80%" />
-                                <h4>{shoes[i].title}</h4>
-                                <div>{shoes[i].price}</div>
-                            </Col>
-                        )
-                    })
-                }
-            </Row>
-        </Container>
+        <div>
+            <Container>
+                <h3>
+                    Example heading <Badge bg="secondary">New</Badge>
+                </h3>
+                <Row>
+                    {
+                        shoes.map((a, i) => {
+                            return (
+                                <Col>
+                                    <img src={'https://codingapple1.github.io/shop/shoes' + (i + 1) + '.jpg'} width="80%" />
+                                    <h4>{shoes[i].title}</h4>
+                                    <div>{shoes[i].price}</div>
+                                </Col>
+                            )
+                        })
+                    }
+                </Row>
+            </Container>
+            <br></br>
+            <br></br>
+            <br></br>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
