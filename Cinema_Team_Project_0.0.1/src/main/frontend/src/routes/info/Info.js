4f4017187a3f2b0be2421ba97c596613b5d68a23
DESKTOP-5UVG7U6\kwon <gurdud0328@naver.com>
2024-07-01 12:13:28 +0900

    css 적용 해보는중

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/info/Info.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/info/Info.js
@@ -2,69 +2,69 @@ import styles from './style/Info.module.css';
 
 function Info() {
     return (
-        <div className='contaniner'>
-            <div className='contents'>
-                <div className='select_main'>
-                    <div className='sect-base-movie'>
-                        <div className='box-imge'>
+        <div className={styles.contaniner}>
+            <div className={styles.contents}>
+                <div className={styles.select_main}>
+                    <div className={styles['sect-base-movie']}>
+                        <div className={styles['box-imge']}>
                         </div>
-                        <div className='box-contents'>
-                            <div className='title'>
+                        <div className={styles['box-contents']}>
+                            <div className={styles.title}>
                                 영화제목
                             </div>
-                            <div className='score'>
+                            <div className={styles.score}>
                                 예매율 및 평점
                             </div>
-                            <div className='spec'>
+                            <div className={styles.spec}>
                                 {/* 영화 상세정보 */}
                                 감독
                                 배우
                                 날짜
                             </div>
-                            <span className='ticketing'>
+                            <span className={styles.ticketing}>
                                 {/* 티켓예매 링크(버튼으로) */}
                             </span>
                         </div>
                     </div>
-                    <div className='cols-content'>
+                    <div className={styles['cols-content']}>
                         {/* 하단 박스 */}
-                        <div className='col-detail'>
-                            <div className='sect-story-movie'>
+                        <div className={styles['col-detail']}>
+                            <div className={styles['sect-story-movie']}>
                                 줄거리
                             </div>
-                            <div className='movie-detail-ad'>
+                            <div className={styles['movie-detail-ad']}>
                                 하단광고
                             </div>
-                            <div className='sect-trailer'>
+                            <div className={styles['sect-trailer']}>
                                 트레일러 영상
                             </div>
-                            <div className='sect-stillcut'>
+                            <div className={styles['sect-stillcut']}>
                                 스틸컷
                             </div>
-                            <div className='sect-grade'>
+                            <div className={styles['sect-grade']}>
                                 {/* 하단 박스 안에 박스(로그인 평가 댓글 기능) */}
-                                <div className='movie-grade'>
+                                <div className={styles['movie-grade']}>
                                     영화 평점
                                 </div>
-                                <div className='real-rating'>
+                                <div className={styles['real-rating']}>
                                     평점작성
                                 </div>
-                                <div className='wrap-persongrade'>
+                                <div className={styles['wrap-persongrade']}>
                                     댓글기능
                                 </div>
                             </div>
                         </div>
-                        <div className='col-aside'>
+                        <div className={styles['col-aside']}>
                             {/* 오른쪽 광고창 */}
-                            <div className='ad-externa'>
-                               오른쪽 광고
+                            <div className={styles['ad-externa']}>
+                                오른쪽 광고
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Info;
