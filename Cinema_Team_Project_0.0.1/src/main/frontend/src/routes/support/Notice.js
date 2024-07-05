218a074f2dd6b40819a119d2e017e2f3f5322755
DuckchiS <rjsdn1862@daum.net>
2024-07-05 22:45:17 +0900

    24.07.05 - 김건우

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/support/Notice.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/support/Notice.js
@@ -4,9 +4,11 @@ import styles from './style/Support.module.css';
 import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 
 const mockNotices = [
-  { id: 1, title: '공지사항 1', content: '공지사항 내용 1' },
-  { id: 2, title: '공지사항 2', content: '공지사항 내용 2' },
-  { id: 3, title: '공지사항 3', content: '공지사항 내용 3' },
+  { id: 1, title: '공지사항 1', content: '공지사항 내용 1', date: '2024-07-05', hit: '0' },
+  { id: 2, title: '공지사항 2', content: '공지사항 내용 2', date: '2024-07-05', hit: '0' },
+  { id: 3, title: '공지사항 3', content: '공지사항 내용 3', date: '2024-07-05', hit: '0' },
+  { id: 4, title: '공지사항 4', content: '공지사항 내용 4', date: '2024-07-05', hit: '0' },
+  { id: 5, title: '공지사항 5', content: '공지사항 내용 5', date: '2024-07-05', hit: '0' },
 ];
 
 function Notice(){
@@ -22,14 +24,36 @@ function Notice(){
 				</ul>
       </div>
       <div className={styles.tableArea}>
-        <ul>
-          {mockNotices.map(notice => (
-            <li key={notice.id}>
-              <h3>{notice.title}</h3>
-              <p>{notice.content}</p>
-            </li>
-          ))}
-        </ul>
+      <table>
+          <thead>
+            <tr>
+              <td>
+                글번호
+              </td>
+              <td>
+                제목
+              </td>
+              <td>
+                날짜
+              </td>
+              <td>
+                조회수
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {mockNotices.map(notice => (
+              <tr key={notice.id}>
+                <td>{notice.id}</td>
+                <td onClick={() => navigate(`/support/notice/${notice.id}`)} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
+                  {notice.title}
+                </td>
+                <td>{notice.date}</td>
+                <td>{notice.hit}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
       <div className={styles.paging}>
           <p>페이징 블록</p>
--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/support/Question.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/support/Question.js
@@ -4,9 +4,11 @@ import styles from './style/Support.module.css';
 import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 
 const mockQuestions = [
-  { id: 1, title: '질문 1', content: '질문 내용 1' },
-  { id: 2, title: '질문 2', content: '질문 내용 2' },
-  { id: 3, title: '질문 3', content: '질문 내용 3' },
+  { id: 1, title: '질문 1', content: '질문 내용 1', date: '2024-07-05', hit: '0' },
+  { id: 2, title: '질문 2', content: '질문 내용 2', date: '2024-07-05', hit: '0' },
+  { id: 3, title: '질문 3', content: '질문 내용 3', date: '2024-07-05', hit: '0' },
+  { id: 4, title: '질문 4', content: '질문 내용 4', date: '2024-07-05', hit: '0' },
+  { id: 5, title: '질문 5', content: '질문 내용 5', date: '2024-07-05', hit: '0' },
 ];
 
 function Question(){
@@ -22,14 +24,36 @@ function Question(){
 				</ul>
       </div>
       <div className={styles.tableArea}>
-        <ul>
-          {mockQuestions.map(notice => (
-            <li key={notice.id}>
-              <h3>{notice.title}</h3>
-              <p>{notice.content}</p>
-            </li>
-          ))}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <td>
+                글번호
+              </td>
+              <td>
+                제목
+              </td>
+              <td>
+                날짜
+              </td>
+              <td>
+                조회수
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {mockQuestions.map(notice => (
+              <tr key={notice.id}>
+                <td>{notice.id}</td>
+                <td onClick={() => navigate(`/support/notice/${notice.id}`)} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
+                  {notice.title}
+                </td>
+                <td>{notice.date}</td>
+                <td>{notice.hit}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
       <div className={styles.paging}>
           <p>페이징 블록</p>
