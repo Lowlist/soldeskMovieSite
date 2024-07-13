0f2797b9ef0d57162d48db88be199307caa7c33d
dud5825@naver.com <dud5825@naver.com>
2024-07-13 20:10:38 +0900

    [조영민] 2024-07-14 -Shop Logic 수정
    Lowlist/soldeskMovieSite#3

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/shop/Shop.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/shop/Shop.js
@@ -7,9 +7,6 @@ import { changeView } from '../../slice/shopSlice.js';
 function Shop() {
     let state = useSelector((state)=>{ return state })
     let disPatch = useDispatch();
-    useEffect(()=>{
-        return ()=>{ disPatch(changeView({package : false})) }
-    },[])
     return (
         <div className="store-main">
             헤더 추가예정
