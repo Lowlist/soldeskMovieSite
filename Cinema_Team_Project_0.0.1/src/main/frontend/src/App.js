a321aa720d453745aa376695d8a0e083edef1b25
DESKTOP-5UVG7U6\kwon <gurdud0328@naver.com>
2024-07-14 21:05:08 +0900

    [권혁영] 영화목록 페이지 생성

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/App.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Info from './routes/info/Info.js';
+import Index from './routes/info/Index.js';
 
 // 내부 스테이트 들은 알아서 만들고 알아서 정리하세요!
 // 공용스테이트같은 경우에는 redux사용해서 정리할것!
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       {/* <span onClick={ ()=>{ c(따봉+1) } }>따봉</span>{따봉} */}
-      <Info></Info>
+      <Index></Index>
     </div>
   );
 }
---
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/info/Index.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import styles from './style/Index.module.css';
+
+function Index() {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Index;
