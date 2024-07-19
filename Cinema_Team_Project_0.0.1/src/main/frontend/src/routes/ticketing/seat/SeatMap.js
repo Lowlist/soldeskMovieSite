113e208fe294fa3d1de58a0f66b597c23e64f417
안재원 <dsfs3975@naver.com>
2024-07-19 12:27:32 +0900

    [안재원]  - 좌석 배열 수정  - 스크린 추가
    Lowlist/soldeskMovieSite#2

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/ticketing/seat/SeatMap.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/ticketing/seat/SeatMap.js
@@ -6,17 +6,20 @@ const SeatMap = ({ selectedSeats, handleSeatClick, hallConfigurations }) => {
     const { rows, cols } = hallConfigurations;
 
     return (
-        <div className={styles.seatMapContainer}>
-            {rows.map((row, rowIndex) => (
-                <SeatRow
-                    key={rowIndex}
-                    row={row}
-                    cols={cols}
-                    selectedSeats={selectedSeats}
-                    handleSeatClick={handleSeatClick}
-                    contentContainerStyle={{paddingHorizontal: 25}}
-                />
-            ))}
+        <div>
+            <div className={styles.screen}>스크린</div>
+            <div className={styles.seatMapContainer}>
+                {rows.map((row, rowIndex) => (
+                    <SeatRow
+                        key={rowIndex}
+                        row={row}
+                        cols={cols}
+                        selectedSeats={selectedSeats}
+                        handleSeatClick={handleSeatClick}
+                        contentContainerStyle={{paddingHorizontal: 25}}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
