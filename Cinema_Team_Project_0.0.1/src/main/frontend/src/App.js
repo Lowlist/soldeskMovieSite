f5ee19cabebdd8d9de4259b2817386ee9fd69e58
DESKTOP-5UVG7U6\kwon <gurdud0328@naver.com>
2024-07-19 15:05:15 +0900

    [권혁영] 2024-07-19 App에서 경로에 오류가 주기적으로 나던 문제, 같은 이름의 js가 존재하기 때문에 문제가 발생하던걸 수정완료

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/App.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Index from './routes/info/Index.js';
+import Index from './routes/info/MovieList.js';
 
 // 내부 스테이트 들은 알아서 만들고 알아서 정리하세요!
 // 공용스테이트같은 경우에는 redux사용해서 정리할것!
