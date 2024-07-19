6623ffc176826b07e1bea0bde7b90e082f096242
dud5825@naver.com <dud5825@naver.com>
2024-07-19 14:58:30 +0900

    [조영민] 2024-07-19 - shop detail 추가
    Lowlist/soldeskMovieSite#3

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/shop/GoodsDetail.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/shop/GoodsDetail.js
@@ -1,13 +1,21 @@
-// import style from './style/GoodsDetail.module.css';
+import style from './style/GoodsDetail.module.css';
 
 function GoodsDetail(){
     return(
         <div>
-            <div>
+            <div className={style.detailName}>
                 ㅋㅋ 이름 나와야됨이제
             </div>
             <hr/>
+                <div className={style.detailLine}>
+                    <div className={style.detailImgLine}>
+                        
+                    </div>
+                    <div className={style.detailTextLine}>
+                        글자라인
+                    </div>
 
+                </div>
             <hr/>
         </div>
     )
