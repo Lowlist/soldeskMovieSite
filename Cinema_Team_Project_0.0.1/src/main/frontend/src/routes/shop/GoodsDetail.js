7a395e65f7f41b5ceb605b742bd4436041c130a2
dud5825@naver.com <dud5825@naver.com>
2024-07-22 16:14:10 +0900

    [조영민] 2024-07-22 -Goods Detail CSS 최종완료
    Lowlist/soldeskMovieSite#3

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/shop/GoodsDetail.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/shop/GoodsDetail.js
@@ -49,8 +49,8 @@ function GoodsDetail(){
                         {/* Title Start */}
 
                         <div className={style.detailTitleLine}>
-                            <div className={style.detailTitle}></div>
-                            <div className={style.detailTitleSale}></div>
+                            <div className={style.detailTitle}>62,000원</div>
+                            <div className={style.detailTitleSale}>68,000원</div>
                         </div>
 
                         {/* Content Start */}
@@ -60,23 +60,26 @@ function GoodsDetail(){
                         <div className={style.detailContentLine}>
 
                             <div className={style.detailContentProductLine}>
-                                <div className={style.detailContentProductName}></div>
-                                <div className={style.detailContentProduct}></div>
+                                <b className={style.detailContentProductName}>상품구성</b>
+                                <div className={style.detailContentProduct}>일반 영화 관람권 4매+더블콤보 1개</div>
                             </div>
 
                             <div className={style.detailContentPeriodLine}>
-                                <div className={style.detailContentPeriodName}></div>
-                                <div className={style.detailContentPeriod}></div>
+                                <b className={style.detailContentPeriodName}>유효기간</b>
+                                <div className={style.detailContentPeriod}>
+                                CGV 영화관람권 : 구매일로부터 24개월 이내<br/>
+                                더블콤보 : 구매일로부터 6개월 이내
+                                </div>
                             </div>
 
                             <div className={style.detailContentOriginLine}>
-                                <div className={style.detailContentOriginName}></div>
-                                <div className={style.detailContentOrigin}></div>
+                                <b className={style.detailContentOriginName}>원산지</b>
+                                <div className={style.detailContentOrigin}>팝콘(팍콘 머그러가는 재열)</div>
                             </div>
 
                             <div className={style.detailContentPlaceLine}>
-                                <div className={style.detailContentPlaceName}></div>
-                                <div className={style.detailContentPlace}></div>
+                                <b className={style.detailContentPlaceName}>상품교환</b>
+                                <div className={style.detailContentPlace}>사용가능 CGV 보기</div>
                             </div>
 
                         </div>
@@ -87,12 +90,13 @@ function GoodsDetail(){
 
                         <div className={style.detailNumberLine}>
                             <div className={style.detailPriceBox}>
-                                <div className={style.detailPriceMinus}></div>
-                                <div className={style.detailPriceText}></div>
-                                <div className={style.detailPricePlus}></div>
+                                <div className={style.detailPriceMinus}>-</div>
+                                <div className={style.detailPriceText}>1</div>
+                                <div className={style.detailPricePlus}>+</div>
                             </div>
 
                             <div className={style.detailPrice}>
+                                248,000원
                             </div>
                         </div>
                         
@@ -101,12 +105,11 @@ function GoodsDetail(){
                         {/* Total Start */}
 
                         <div className={style.detailTotalLine}>
-                            <div className={style.detailTotalText}>
+                            <b className={style.detailTotalText}>
                                 총 구매금액
-                            </div>
-
+                            </b>
                             <div className={style.detailTotal}>
-
+                                248,000원
                             </div>
                         </div>
 
@@ -114,7 +117,7 @@ function GoodsDetail(){
 
                         <div className={style.detailButtonLine}>
                             <div className={style.detailBasketButton}></div>
-                            <div className={style.detailBuyButton}></div>
+                            <div className={style.detailBuyButton}>구매하기</div>
                         </div>
                         
                     </div>
