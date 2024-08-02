7e5b8ebe67cba0c0616d75b9a393e1e10e31d504
DuckchiS <rjsdn1862@daum.net>
2024-08-02 16:54:03 +0900

    24.08.02 - 김건우

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/support/notice/NoticeApi.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/support/notice/NoticeApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/support/notice';
+const API_URL = 'http://localhost:7080/support/notice';
 
 // 공지사항 목록을 가져오는 함수
 export const getNotices = async () => {
