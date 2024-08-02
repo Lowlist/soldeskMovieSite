18141c23ac9c48dc82d7b826cfa0d3dba4b29045
jin <jin.com>
2024-08-02 18:42:56 +0900

    로그인 간단한거 오늘 10시 좀 넘어서 마저 추가 하겠습니다

--- Cinema_Team_Project_0.0.1/src/main/java/com/team/cinema/member/entity/AuthVO.java
+++ Cinema_Team_Project_0.0.1/src/main/java/com/team/cinema/member/entity/AuthVO.java
@@ -1,11 +1,27 @@
 package com.team.cinema.member.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "admin")
 public class AuthVO {
-	private int seq; // 일련번호\
-	private String role;
-	private String memID; //회원 아이디
-	private String auth; // 회원권한(3가지 부여 , ROLE_USER, ROLE_MANAGER, ROLE_ADMIN)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int no;
+    private int role;
+    private String menu;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "no", referencedColumnName = "no", insertable = false, updatable = false)
+    private Member member;
+	
 }
