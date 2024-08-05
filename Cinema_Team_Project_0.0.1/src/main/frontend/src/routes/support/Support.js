a6168190eb883ebba3b71d91972f2d58f682f208
DuckchiS <rjsdn1862@daum.net>
2024-08-05 16:20:53 +0900

    24.08.05 - 김건우(고객센터 html,css 수정)

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/support/Support.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/support/Support.js
@@ -17,7 +17,7 @@ function Support() {
         <MovieHeader />
         <div className={styles.contents}>
           <div className={styles.colsContent}>
-          <div className={styles.colAsideImageContainerWrapper}>
+            <div className={styles.colAsideImageContainerWrapper}>
               <div className={styles.colAside}>
                 <ul>
                   <li>
@@ -44,50 +44,49 @@ function Support() {
                   <button onClick={() => navigate('question')}>질문 게시판으로 가기</button>
                   <button onClick={() => navigate('question/realtime')}>실시간 문의로 가기</button>
                   <div className={styles['announcement-div']}>
-                  <div className={styles['announcement-body']}>
-                    <div className={styles['announcement-service']}>
-                      <div className={styles['announcement-main']}>
-                        <div className={styles['announcement-title']}>
-                          <strong>공지사항</strong>
+                    <div className={styles['announcement-body']}>
+                      <div className={styles['announcement-service']}>
+                        <div className={styles['announcement-main']}>
+                          <div className={styles['announcement-title']}>
+                            <strong>공지사항</strong>
+                          </div>
+                          <div className={styles['announcement-list']}>
+                            공지사항 리스트 1
+                          </div>
+                          <div className={styles['announcement-button']}>
+                            <div className={styles['announcement-button-click']}>
+                              더보기
+                            </div>
+                          </div>
                         </div>
-                        {/* 공지사항 1줄 데이터 바인딩 */}
-                        <div className={styles['announcement-list']}>
-                          공지사항 리스트 1
-                        </div>
-                        <div className={styles['announcement-button']}>
-                          <div className={styles['announcement-button-click']}>
-                            더보기
+                        <div className={styles['service-center']}>
+                          <div className={styles['service-body']}>
+                            <div className={styles['service-title']}>
+                              <strong>고객센터</strong>
+                            </div>
+                            <div className={styles['service-subdiv']}>
+                              <div className={styles['service-number']}>
+                                1544-1122
+                              </div>
+                              <div className={styles['service-time']}>
+                                고객센터 운영시간 {'('}평일 09:00 ~ 18:00{')'}
+                              </div>
+                              <div className={styles['service-info']}>
+                                업무시간 외 자동응답 안내 가능합니다.
+                              </div>
+                            </div>
+                          </div>
+                          <div className={styles['service-footer']}>
+                            <div className={styles['service-button-div']}>
+                              <button className={styles['service-button']}>FAQ</button>
+                            </div>
+                            <div className={styles['service-button-div2']}>
+                              <button className={styles['service-button2']}>1:1문의</button>
+                            </div>
                           </div>
                         </div>
                       </div>
-                      <div className={styles['service-center']}>
-                        <div className={styles['service-body']}>
-                          <div className={styles['service-title']}>
-                            <strong>고객센터</strong>
-                          </div>
-                          <div className={styles['service-subdiv']}>
-                            <div className={styles['service-number']}>
-                              1544-1122
-                            </div>
-                            <div className={styles['service-time']}>
-                              고객센터 운영시간 {'('}평일 09:00 ~ 18:00{')'}
-                            </div>
-                            <div className={styles['service-info']}>
-                              업무시간 외 자동응답 안내 가능합니다.
-                            </div>
-                          </div>
-                        </div>
-                        <div className={styles['service-footer']}>
-                          <div className={styles['service-button-div']}>
-                            <button className={styles['service-button']}>FAQ</button>
-                          </div>
-                          <div className={styles['service-button-div2']}>
-                            <button className={styles['service-button2']}>1:1문의</button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className={styles['app-download']}>
+                      <div className={styles['app-download']}>
                         <div className={styles['app-div']}>
                           <div className={styles['app-title']}>
                             <strong>앱 다운로드</strong>
@@ -100,7 +99,7 @@ function Support() {
                           </div>
                           <div className={styles['qr-info']}>
                             QR코드를 스캔하고
-                            <br></br>
+                            <br />
                             앱설치 페이지로 바로 이동하세요
                           </div>
                         </div>
@@ -109,7 +108,7 @@ function Support() {
                   </div>
                 </>
               )}
-              <Outlet />             
+              {!isRootPath && <Outlet />}
             </div>
           </div>
         </div>
